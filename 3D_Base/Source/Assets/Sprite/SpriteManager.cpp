96e82277b934723943494a183527b66c93501382
Sato <2410048@i-seifu.jp>
2025-08-04 10:20:00 +0900

    SpriteManager改良

--- 3D_Base/Source/Assets/Sprite/SpriteManager.cpp
+++ 3D_Base/Source/Assets/Sprite/SpriteManager.cpp
@@ -4,8 +4,8 @@
 SpriteManager::SpriteManager()
 	: m_p2DSpritMap()
 	, m_p3DSpritMap()
-	, m_Sprite2DMeshInfoList()
-	, m_Sprite3DMeshInfoList()
+	, m_Sprite2DInfoList()
+	, m_Sprite3DInfoList()
 {
 }
 
@@ -13,12 +13,16 @@ SpriteManager::~SpriteManager()
 {
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 HRESULT SpriteManager::Create()
 {
-	if (CreateSprite2D() != S_OK) { return E_FAIL; }
-	if (CreateSprite3D() != S_OK) { return E_FAIL; }
+	if (CreateSprite2D() != S_OK) { return E_POINTER; }
+	if (CreateSprite3D() != S_OK) { return E_POINTER; }
 	return S_OK;
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 HRESULT SpriteManager::LoadData()
 {
@@ -27,16 +31,19 @@ HRESULT SpriteManager::LoadData()
 	return S_OK;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 HRESULT SpriteManager::CreateSprite3D()
 {
-	//各3Dスプライトの情報を登録(ここに3Dスプライト登録)
+	//各sprite3Dの情報を登録(ここにsprite3D登録)
 //↓-----------------------------------------------------------------------------------------------------------------------------↓
 	RegisterSprite3D(Sprite3DList::Ground,		_T("Data\\Texture\\Ground.png"),	{ 1.f, 1.f, 256.f, 256.f, 256.f, 256.f	});
 	RegisterSprite3D(Sprite3DList::Player,		_T("Data\\Texture\\Player.png"),	{ 1.f, 1.f, 64.f, 64.f, 64.f, 64.f		});
 	RegisterSprite3D(Sprite3DList::Explosion,	_T("Data\\Texture\\explosion.png"), { 1.f, 1.f, 256.f, 256.f, 32.f, 32.f	});
 //↑-----------------------------------------------------------------------------------------------------------------------------↑
 
-	for (auto& sprite3D : m_Sprite3DMeshInfoList)
+	//登録したsprite3Dのインスタンス作成.
+	for (auto& sprite3D : m_Sprite3DInfoList)
 	{
 		m_p3DSpritMap[sprite3D.Id] = std::make_unique<CSprite3D>();
 		if (!m_p3DSpritMap[sprite3D.Id]) return E_POINTER;
@@ -45,15 +52,17 @@ HRESULT SpriteManager::CreateSprite3D()
 	return S_OK;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 HRESULT SpriteManager::CreateSprite2D()
 {
-	//各2Dスプライトの情報を登録(ここに2Dスプライト登録)
+	//各sprite2Dの情報を登録(ここにsprite2D登録)
 //↓-----------------------------------------------------------------------------------------------------------------------------↓
 	RegisterSprite2D(Sprite2DList::PMon,_T("Data\\Texture\\pmon.png"),{ 64.f, 64.f, 896.f, 560.f, 896.f / 16.f, 560.f / 10.f });
 //↑-----------------------------------------------------------------------------------------------------------------------------↑
 
-	//各2Dスプライトのインスタンス作成.
-	for (auto& sprite2D : m_Sprite2DMeshInfoList)
+	//各sprite2Dのインスタンス作成.
+	for (auto& sprite2D : m_Sprite2DInfoList)
 	{
 		m_p2DSpritMap[sprite2D.Id] = std::make_unique<CSprite2D>();
 		if (!m_p2DSpritMap[sprite2D.Id]) return E_POINTER;
@@ -62,10 +71,12 @@ HRESULT SpriteManager::CreateSprite2D()
 	return S_OK;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 HRESULT SpriteManager::LoadSprite2D()
 {
-	//2Dスプライト読み込み
-	for (auto& sprite2D : m_Sprite2DMeshInfoList)
+	//sprite2D読み込み
+	for (auto& sprite2D : m_Sprite2DInfoList)
 	{
 		if(m_p2DSpritMap[sprite2D.Id]->Init(sprite2D.Path, sprite2D.Size) != S_OK)
 		return E_FAIL;
@@ -74,10 +85,12 @@ HRESULT SpriteManager::LoadSprite2D()
 	return S_OK;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 HRESULT SpriteManager::LoadSprite3D()
 {
-	//3Dスプライト読み込み
-	for (auto& sprite3D : m_Sprite3DMeshInfoList)
+	//sprite3D読み込み
+	for (auto& sprite3D : m_Sprite3DInfoList)
 	{
 		if (m_p3DSpritMap[sprite3D.Id]->Init(sprite3D.Path, sprite3D.Size) != S_OK)
 			return E_FAIL;
@@ -85,6 +98,8 @@ HRESULT SpriteManager::LoadSprite3D()
 
 	return S_OK;
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 std::shared_ptr<CSprite3D> SpriteManager::GetSprite3D(Sprite3DList spriteID)
 {
@@ -96,6 +111,8 @@ std::shared_ptr<CSprite3D> SpriteManager::GetSprite3D(Sprite3DList spriteID)
 	return nullptr;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 std::shared_ptr<CSprite2D> SpriteManager::GetSprite2D(Sprite2DList spriteID)
 {
 	auto it = m_p2DSpritMap.find(spriteID);
@@ -106,12 +123,16 @@ std::shared_ptr<CSprite2D> SpriteManager::GetSprite2D(Sprite2DList spriteID)
 	return nullptr;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void SpriteManager::RegisterSprite2D(Sprite2DList spriteID, LPCTSTR path, CSprite2D::SPRITE_STATE size)
 {
-	m_Sprite2DMeshInfoList.push_back({ spriteID, path, size });
+	m_Sprite2DInfoList.push_back({ spriteID, path, size });
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void SpriteManager::RegisterSprite3D(Sprite3DList spriteID, LPCTSTR path, CSprite3D::SPRITE_STATE size)
 {
-	m_Sprite3DMeshInfoList.push_back({ spriteID, path, size });
+	m_Sprite3DInfoList.push_back({ spriteID, path, size });
 }
