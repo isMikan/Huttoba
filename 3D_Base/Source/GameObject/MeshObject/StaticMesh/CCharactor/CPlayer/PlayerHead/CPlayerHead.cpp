505b4ba57980290017e127323eb977138591d041
gappa <2410007@i-seifu.jp>
2025-09-05 14:38:54 +0900

    プレイヤー関連のファイルを増やした

---
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/CCharactor/CPlayer/PlayerHead/CPlayerHead.cpp
@@ -0,0 +1,20 @@
+#include "CPlayerHead.h"
+
+CPlayerHead::CPlayerHead()
+{
+	m_OffsetPos = D3DXVECTOR3( 0.f, 1.f, 0.f );
+}
+
+CPlayerHead::~CPlayerHead()
+{
+}
+
+void CPlayerHead::Update()
+{
+	CCharacter::Update();
+}
+
+void CPlayerHead::Draw(D3DXMATRIX& View, D3DXMATRIX& Proj, LIGHT& Light, CAMERA& Camera)
+{
+	CCharacter::Draw(View, Proj, Light, Camera);
+}
