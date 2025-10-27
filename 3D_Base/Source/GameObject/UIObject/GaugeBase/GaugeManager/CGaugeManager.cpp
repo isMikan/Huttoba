0dc7458c49bfe998f8c043f8d81df4efca76aae2
gappa <2410007@i-seifu.jp>
2025-10-27 13:57:24 +0900

    ゲージを各プレイヤーに割り当てる

--- 3D_Base/Source/GameObject/UIObject/GaugeBase/GaugeManager/CGaugeManager.cpp
+++ 3D_Base/Source/GameObject/UIObject/GaugeBase/GaugeManager/CGaugeManager.cpp
@@ -8,6 +8,8 @@
 CGaugeManager::CGaugeManager()
 	: m_pGauge				()
 {
+	m_pGauge.clear();
+	m_pGauge.resize(Gauge_Max);
 }
 
 CGaugeManager::~CGaugeManager()
@@ -23,108 +25,101 @@ CGaugeManager::~CGaugeManager()
 void CGaugeManager::Create(CPlayerManager* playerManager)
 {
 	//ゲージのインスタンス作成.
-	m_pGauge.clear();
-	m_pGauge.resize(Gauge_Max);
-
 	for (int pNo = 0; pNo < Player_Max; pNo++)
 	{
 		CPlayerBase* player = playerManager->GetPlayer(pNo);
 		if (!player) continue;	//存在しなかったら次へ.
 
 		auto& bus = player->GetBus();
+		//購買処理.
 		bus.Subscribe([this, player](CPlayerState* state) {
 			if (dynamic_cast<CPlayerKnockdownState*>(state))
 			{
-				if (m_SubscribePlayers.contains(player)) return;
-
-				m_SubscribePlayers.insert(player);
-
-				for (int gNo = 0; gNo < Gauge_Max; gNo++)
+				for (int frameNo = 0; frameNo < Gauge_Max; frameNo++)
 				{
-					if (m_pGauge[gNo]) continue;	//作成されていたら次へ.
+					if (m_SubscribePlayers.contains(player)) return;
 
-					int nextId = gNo + 1;
+					if (m_pGauge[frameNo]) continue;	//作成されていたら次へ.
+
+					int gaugeNo = frameNo + 1;
 
 					//ゲージフレームの作成.
-					m_pGauge[gNo] = std::make_unique<CGaugeFrame>();
-					m_pGauge[gNo]->AttachSprite(AssetManager::Sprite(Sprite2DList::GaugeFrame));
-					std::cout << typeid(m_pGauge[gNo].get()).name() << std::endl;
+					m_pGauge[frameNo] = std::make_unique<CGaugeFrame>();
+					m_pGauge[frameNo]->AttachSprite(AssetManager::Sprite(Sprite2DList::GaugeFrame));
+					std::cout << typeid(m_pGauge[frameNo].get()).name() << std::endl;
 
 					//ゲージの作成.
-					m_pGauge[nextId] = std::make_unique<CTimerGauge>();
-					m_pGauge[nextId]->AttachSprite(AssetManager::Sprite(Sprite2DList::Gauge));
-					m_pGauge[nextId]->SetGaugeInfo(player->GetKnockdownTime());
-					std::cout << typeid(m_pGauge[gNo].get()).name() << std::endl;
+					m_pGauge[gaugeNo] = std::make_unique<CTimerGauge>();
+					m_pGauge[gaugeNo]->AttachSprite(AssetManager::Sprite(Sprite2DList::Gauge));
+					m_pGauge[gaugeNo]->SetGaugeInfo(player->GetKnockdownTime());
+					std::cout << typeid(m_pGauge[frameNo].get()).name() << std::endl;
 
 					std::cout << "ゲージを作成" << player->GetPlayerID() <<  std::endl;
+
+					m_SubscribePlayers.insert(player);
+
+					m_PlayerGauge[player] = frameNo;
+
+					break;	//1つだけゲージを作成.
 				}
 			}
 			else
 			{
-				//m_SubscribePlayers.erase(player);
-
-				for (int gNo = 0; gNo < Gauge_Max; gNo++)
+				auto playerGauge = m_PlayerGauge.find(player);
+				if (playerGauge != m_PlayerGauge.end())
 				{
-					if (!m_pGauge[gNo]) continue;	//作成いなかったら次へ.
+					int frameNo = playerGauge->second;	//フレーム番号.
+					int gaugeNo = frameNo + 1;			//ゲージ番号.
 
-					int nextId = gNo + 1;
-					m_pGauge[gNo].reset();
-					m_pGauge[nextId].reset();
+					m_pGauge[frameNo].reset();
+					m_pGauge[gaugeNo].reset();
+
+					m_SubscribePlayers.erase(player);
+					m_PlayerGauge.erase(playerGauge);
 				}
 			}
 		});
 	}
 }
 
-//--- 読込関数 ---.
-void CGaugeManager::LoadData(CPlayerManager* playerManager)
-{
-	for (int gNo = 0; gNo < Gauge_Max; gNo++)
-	{
-		if (gNo % 2 == 0)
-		{
-			m_pGauge[gNo] = std::make_unique<CGaugeFrame>();
-			//ゲージフレームのスプライトを設定.
-			m_pGauge[gNo]->AttachSprite(AssetManager::Sprite(Sprite2DList::GaugeFrame));
-		}
-		else
-		{
-			//ゲージスプライトを設定.
-			m_pGauge[gNo]->AttachSprite(AssetManager::Sprite(Sprite2DList::Gauge));
-		}
-	}
-}
-
 //--- 破棄関数 ---.
 void CGaugeManager::Destroy()
 {
+	m_SubscribePlayers.clear();
+	m_PlayerGauge.clear();
 	for (auto& gauge : m_pGauge)
 	{
 		gauge.reset();
 	}
+	m_pGauge.clear();
 }
 
 //--- 更新処理 ---.
 void CGaugeManager::Update(CPlayerManager* playerManager)
 {
-	for (int pNo = 0; pNo < Player_Max; pNo++)
+	for (const auto& pair : m_PlayerGauge)
 	{
-		CPlayerBase* player = playerManager->GetPlayer(pNo);
+		CPlayerBase* player = pair.first;	//プレイヤー.
+		int frameNo = pair.second;			//フレーム番号.
+		int gaugeNo = frameNo + 1;			//ゲージ番号.
 
-		if (!player) continue;	//存在しなかったら次へ.
+		if (!m_pGauge[frameNo] || !m_pGauge[gaugeNo]) continue;
 
-		for (int gNo = 0; gNo < Gauge_Max; gNo++)
+		//タイムゲージクラスの場合.
+		if (dynamic_cast<CTimerGauge*>(m_pGauge[gaugeNo].get()))
 		{
-			if (!m_pGauge[gNo]) continue;
-
-			if (dynamic_cast<CTimerGauge*>(m_pGauge[gNo].get()))
-			{
-				m_pGauge[gNo]->SetGaugeInfo(player->GetKnockdownTime());
-			}
-			m_pGauge[gNo]->Update();
-			m_pGauge[gNo]->SetWorldPos(player->GetPosition());
-			//std::cout << typeid(player).name() << pNo << gNo << std::endl;
+			//時間を取得し、ゲージクラスに渡す.
+			m_pGauge[gaugeNo]->SetGaugeInfo(player->GetKnockdownTime());
 		}
+		//フレーム.
+		m_pGauge[frameNo]->Update();	//更新.
+		m_pGauge[frameNo]->SetWorldPos(player->GetPosition());	//世界座標を設定.
+
+		//ゲージ.
+		m_pGauge[gaugeNo]->Update();	//更新.
+		m_pGauge[gaugeNo]->SetWorldPos(player->GetPosition());	//世界座標を設定.
+
+		std::cout << typeid(player).name() << gaugeNo << std::endl;
 	}
 }
 
--- 3D_Base/Source/GameObject/UIObject/GaugeBase/GaugeManager/CGaugeManager.h
+++ 3D_Base/Source/GameObject/UIObject/GaugeBase/GaugeManager/CGaugeManager.h
@@ -22,9 +22,6 @@ public:
 	//--- 構築関数 ---.
 	void Create(
 		CPlayerManager* playerManager);
-	//--- 読込関数 ---.
-	void LoadData(
-		CPlayerManager* playerManager);
 	//--- 破棄関数 ---.
 	void Destroy();
 	//--- 更新関数 ---.
@@ -38,7 +35,8 @@ protected:
 //======================================================================
 // 	   内部で使用する変数.
 //======================================================================
-	std::vector<std::unique_ptr<CGaugeBase>>	m_pGauge;	//ゲージ.
+	std::vector<std::unique_ptr<CGaugeBase>>	m_pGauge;			//ゲージ.
 
-	std::unordered_set<CPlayerBase*>	m_SubscribePlayers;	//プレイヤー.
+	std::unordered_map<CPlayerBase*, int>		m_PlayerGauge;		//プレイヤーとゲージを照らし合わせる.	
+	std::unordered_set<CPlayerBase*>			m_SubscribePlayers;	//プレイヤーの購買リスト.
 };
