2bfbc22636e2fc4cc9b62a44b4c14598b1b124a1
R200-02\game <2420061@i-seifu.jp>
2025-11-06 17:50:01 +0900

    エラー修正

--- 3D_Base/Source/GameObject/MeshObject/StaticMesh/Item/Items/TrackingRobot/TrackingRobot.cpp
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/Item/Items/TrackingRobot/TrackingRobot.cpp
@@ -224,7 +224,7 @@ void TrackingRobot::Smash(CPlayer& playiers)
 
 	D3DXVECTOR3 SmashVel = playiers.GetVelocity(m_vPosition, CalculateForceScalar(len), 60.0f);
 
-	playiers.SetHitInfo(
+	playiers.SetHitAttack(
 		SmashVel,
 		CPlayerBase::HitEvent::Knockdown);
 }
