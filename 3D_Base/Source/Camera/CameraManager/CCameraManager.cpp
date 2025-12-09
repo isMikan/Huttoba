9d165733f2a754cde04e455b2b696eb383a65945
R200-03\game <2410007@i-seifu.jp>
2025-12-09 16:23:47 +0900

    戻した

--- 3D_Base/Source/Camera/CameraManager/CCameraManager.cpp
+++ 3D_Base/Source/Camera/CameraManager/CCameraManager.cpp
@@ -11,7 +11,7 @@ CCameraManager::CCameraManager()
 {
 	//地面の数分位置を変えるので、地面の最大数を設定.
 	m_LastCamera.resize(Ground_Max);
-	m_LastCamera[0] = { D3DXVECTOR3(0.f, 17.f, 0.f), D3DXVECTOR3(0.f, 1.f, 10.f) };
+	m_LastCamera[0] = { D3DXVECTOR3(0.f, 17.f, -3.f), D3DXVECTOR3(0.f, 1.f, 10.f) };
 	m_LastCamera[1] = { D3DXVECTOR3(0.f, 17.f, -3.f), D3DXVECTOR3(0.f, 1.f, 9.f) };
 	m_LastCamera[2] = { D3DXVECTOR3(0.f, 20.f, -4.f), D3DXVECTOR3(0.f, 1.f, 8.f) };
 	m_LastCamera[3] = { D3DXVECTOR3(0.f, 20.f, -10.f), D3DXVECTOR3(0.f, 1.f, 6.f) };
--- 3D_Base/Source/GameObject/MeshObject/StaticMesh/Ground/GroundManager/CGroundManager.cpp
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/Ground/GroundManager/CGroundManager.cpp
@@ -5,8 +5,8 @@ CGroundManager::CGroundManager()
 {
 	Create();
 	//\
+	m_FallTime = { 99.f, 45.f, 30.f, 15.f };
 	m_FallTime = { 999.f, 999.f, 999.f, 999.f };
-	m_FallTime = { 99.f, 15.f, 10.f, 5.f };
 
 	ExtractMeshRadius();
 }
