381a8b5154c1f5b2cbbd401bece6a20493c513d9
R200-03\game <2410007@i-seifu.jp>
2025-12-09 16:16:25 +0900

    カメラの調整

--- 3D_Base/Source/GameObject/MeshObject/StaticMesh/Ground/GroundManager/CGroundManager.cpp
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/Ground/GroundManager/CGroundManager.cpp
@@ -6,7 +6,7 @@ CGroundManager::CGroundManager()
 	Create();
 	//\
 	m_FallTime = { 999.f, 999.f, 999.f, 999.f };
-	m_FallTime = { 99.f, 45.f, 30.f, 10.f };
+	m_FallTime = { 99.f, 15.f, 10.f, 5.f };
 
 	ExtractMeshRadius();
 }
