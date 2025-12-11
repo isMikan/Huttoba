f087d7750c50a89b4ef5723f9ce9d185b1394387
R200-03\game <2410007@i-seifu.jp>
2025-12-11 16:43:30 +0900

    カメラ角度

--- 3D_Base/Source/Camera/CameraManager/CCameraManager.cpp
+++ 3D_Base/Source/Camera/CameraManager/CCameraManager.cpp
@@ -14,7 +14,7 @@ CCameraManager::CCameraManager()
 	m_LastCamera[0] = { D3DXVECTOR3(0.f, 17.f, -3.f), D3DXVECTOR3(0.f, 1.f, 10.f) };
 	m_LastCamera[1] = { D3DXVECTOR3(0.f, 17.f, -3.f), D3DXVECTOR3(0.f, 1.f, 9.f) };
 	m_LastCamera[2] = { D3DXVECTOR3(0.f, 20.f, -4.f), D3DXVECTOR3(0.f, 1.f, 8.f) };
-	m_LastCamera[3] = { D3DXVECTOR3(0.f, 20.f, -10.f), D3DXVECTOR3(0.f, 1.f, 6.f) };
+	m_LastCamera[3] = { D3DXVECTOR3(0.f, 26.f, -6.f), D3DXVECTOR3(0.f, 2.f, 7.f) };
 }
 
 CCameraManager::~CCameraManager()
