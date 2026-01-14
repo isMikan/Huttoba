5c087b689ed9913cec119d5524c61d98c8750f3d
R200-02\game <2420061@i-seifu.jp>
2026-01-14 16:13:31 +0900

    修正とレディーゴーが表示されるように

--- 3D_Base/Source/GameObject/MeshObject/StaticMesh/Item/Items/TrackingRobot/TrackingRobot.cpp
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/Item/Items/TrackingRobot/TrackingRobot.cpp
@@ -30,7 +30,7 @@ TrackingRobot::TrackingRobot()
 
 	, m_IsExploded		( false )
 
-	, m_Is				( false )
+	, m_IsPendingDestroy( false )
 
 	, m_ExplosionRadius	()
 
@@ -147,12 +147,12 @@ void TrackingRobot::OnCollision(CollisionBase* other)
 			//爆発したときだけ判定する
 			if (m_IsExploded)
 			{
-				m_Is = true;
 				Smash(*player);
 			}
 
 			if (m_State == State::Use)
 			{
+				//使用したプレイヤー以外に当たった際の処理
 				if (m_pPlayer != player)
 					Explosion();
 			}
@@ -179,11 +179,15 @@ void TrackingRobot::HaveMove()
 void TrackingRobot::UseMove()
 {
 	//爆発すればアイテムを破棄する
-	if (m_Is)
+	if (m_IsPendingDestroy)
 	{
 		m_IsDestroy = true;
 		return;
 	}
+
+	if (m_IsExploded)
+		m_IsPendingDestroy = true;
+
 
 	if(m_IsOnGround)
 	{
@@ -206,6 +210,7 @@ void TrackingRobot::UseMove()
 			//取り出したZ軸成分をノーマライズ
 			D3DXVec3Normalize(&forward, &forward);
 
+			//向いている方向に移動する
 			m_Velocity = forward * m_MoveSpeed;
 		}
 
--- 3D_Base/Source/Scene/SceneMain/CSceneGameMain.h
+++ 3D_Base/Source/Scene/SceneMain/CSceneGameMain.h
@@ -72,8 +72,12 @@ private:
 	//進行状況の変化用タイマー
 	float		m_StateTimer;
 
-	//レディからプレイに遷移するまでの時間
+	//レディからゴーに遷移するまでの時間
 	float		m_ReadyDuration;
+
+	//ゴーからプレイに遷移するまでの時間
+	float		m_GoDuration;
+
 	//フィニッシュから次の画面に遷移するまでの時間
 	float		m_FinishDuration;
 
