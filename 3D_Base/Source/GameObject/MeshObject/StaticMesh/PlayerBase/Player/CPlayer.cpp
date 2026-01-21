e9c5e7f261688090a0bef33e04ada2847c384984
R200-03\game <2410007@i-seifu.jp>
2026-01-21 10:01:34 +0900

    攻撃A、拾うX

--- 3D_Base/Source/GameObject/MeshObject/StaticMesh/PlayerBase/Player/CPlayer.cpp
+++ 3D_Base/Source/GameObject/MeshObject/StaticMesh/PlayerBase/Player/CPlayer.cpp
@@ -171,8 +171,8 @@ void CPlayer::SetPlayerInputBinding(int index) const
 
 	//ƒRƒ“ƒgƒ[ƒ‰‘€ì.
 	CInputManager::BindKey(Action::Attack,
-		InputBinding(InputDevice::GamePad, CXInput::X), m_PlayerID);	//UŒ‚.
+		InputBinding(InputDevice::GamePad, CXInput::A), m_PlayerID);	//UŒ‚.
 
 	CInputManager::BindKey(Action::ToggleItem,	
-		InputBinding(InputDevice::GamePad, CXInput::A), m_PlayerID);	//E‚¤/Ì‚Ä‚é.
+		InputBinding(InputDevice::GamePad, CXInput::X), m_PlayerID);	//E‚¤/Ì‚Ä‚é.
 }
